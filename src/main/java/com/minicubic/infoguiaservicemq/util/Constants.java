b5625631fc316feec659f67e0f4d991787567df9
Hector Villalba <alfredovillalba460@hotmail.com>
2017-08-14 18:35:07 -0400

    Merge origin/master
    Conflicts:
    	src/main/java/com/minicubic/infoguiacore/model/Cliente.java
    	src/main/resources/META-INF/persistence.xml

--- src/main/java/com/minicubic/infoguiaservicemq/util/Constants.java
+++ src/main/java/com/minicubic/infoguiaservicemq/util/Constants.java
@@ -7,8 +7,7 @@ package com.minicubic.infoguiaservicemq.util;
 public class Constants {
     
     public static final String SECRET_KEY = "INFOGUIA-VILO-SA";
-    
     public static final String MQTT_ADDR = "tcp://45.79.159.123:1883";
     public static final String MQTT_USER = "minicubic";
-    public static final String MQTT_PASS = ".minicubic.2016.80*";
+    public static final String MQTT_PASS = ".minicubic.2017*";
 }
---
+++ src/main/java/com/minicubic/infoguiaservicemq/util/TestLocation.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.minicubic.infoguiaservicemq.util;
+
+/**
+ *
+ * @author hectorvillalba
+ */
+public class TestLocation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        long lista = 203;
+        long div = 50;
+        long total ; 
+        System.err.println("total "+  lista/50);
+        System.err.println("resto "+  (lista%50));
+        // TODO code application logic here
+    }
+    
+}
